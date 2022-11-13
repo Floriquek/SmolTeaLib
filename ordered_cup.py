1e575451728f7d710a2cb0b2c2b34f31dd25058e
Florique <95179214+Floriquek@users.noreply.github.com>
2022-11-13 18:21:39 +0200

    ..

--- ordered_cup.py
+++ ordered_cup.py
@@ -11,3 +11,12 @@ def expensive_tea( df: pd.DataFrame) -> pd.DataFrame:
 
 # output expensive cup of tea
 print(expensive_tea(df=_tea_df()))
+
+##
+#Results from expensive tea decorator:
+
+#     Type Price
+#0      NA    NA
+#1  matcha    20
+#2      NA    NA
+#3      NA    NA
