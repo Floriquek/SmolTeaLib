1c460121db40d28a860eb26ae563856777098790
Florique <95179214+Floriquek@users.noreply.github.com>
2022-11-13 18:30:17 +0200

    ..

--- ordered_cup.py
+++ ordered_cup.py
@@ -6,17 +6,18 @@ import pandas as pd
 # apply expensive tea decorator 
 
 @expensive_tea_decorator
-def expensive_tea( df: pd.DataFrame) -> pd.DataFrame:
+def expensive_tea( df: pd.DataFrame ) -> pd.DataFrame:
     return df
 
 # output expensive cup of tea
-print(expensive_tea(df=_tea_df()))
+
+print( expensive_tea(df=_tea_df()) )
 
 ##
 #Results from expensive tea decorator:
 
-#     Type Price
-#0      NA    NA
-#1  matcha    20
-#2      NA    NA
-#3      NA    NA
+#      Type Price
+# 0      NA    NA
+# 1  matcha    20
+# 2      NA    NA
+# 3      NA    NA
